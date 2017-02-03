97c12f3ee1e9c23d17bce052b6ce1c12d18e4df0
Jackie Wu <wkcn@live.cn>
2017-02-03 13:17:45 +0800

    fix avg bug

--- face.py
+++ face.py
@@ -2,6 +2,9 @@ from PIL import Image
 import numpy as np
 from numpy import linalg as LA
 import random
+import operator
+
+TRAINING = False
 
 path = "./orl_faces/"
 
@@ -26,19 +29,26 @@ for i in range(n):
 X = np.matrix(np.zeros((n * 7, w * h)))
 k = 0
 S = np.matrix(np.zeros((n, 10)))
-ps = np.load("s.npy")
+
+if not TRAINING:
+    ps = np.load("s.npy")
+
 for i in range(n):
-    #s = [e for e in range(10)]
-    #random.shuffle(s)
-    s = ps[i, :]
-    #S[i, :] = s 
+    if TRAINING:
+        s = [e for e in range(10)]
+        random.shuffle(s)
+    else:
+        s = ps[i, :]
+
+    S[i, :] = s 
     for j in s[:7]:
         X[k, :] = people[i][int(j)]
         k += 1
 
 def Train():
     #avg 
-    avg = np.mean(X)
+    avg = np.mean(X, axis = 0)
+    print (avg)
     W = X - avg
     e,r = W.shape
     if r <= e:
@@ -52,33 +62,53 @@ def Train():
     np.save("w.npy", pw)
     np.save("v.npy", pv)
     np.save("s.npy", S)
+    np.save("avg.npy", avg)
+
+def knn(su, k):
+    si = su.argsort(axis = 0)
+    classCount = {}
+    for i in range(k):
+        w = si[i, 0] // 7
+        classCount[w] = classCount.get(w, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
 
 def Recognize():
     pw = np.load("w.npy")
     pv = np.load("v.npy")
     ps = np.load("s.npy")
+    avg = np.load("avg.npy")
     #pv = np.eye(w * h)
-    ev = pv[:, :50]
+    ev = pv[:, :30] # feature vector
     # X n * (wh)
     # ev wh * 80 
-    Y = X * ev # 
+    W = X - avg 
+    Y = W * ev # 
     right = 0
+    k = 3
+    avg2 = np.matrix(avg.reshape((w*h, 1)))
     for i in range(n):
         for j in ps[i, 7:]:
             p = np.matrix(people[i][int(j)]).reshape((w * h, 1))
-            y = ev.T * p
+            pa = p - avg2
+            y = ev.T * pa 
             #recognize
             f = np.tile(y.T, (n * 7, 1))
             d = Y - f
             g = np.multiply(d, d)
             su = np.sum(g, axis = 1)
+            #r = knn(su, k)
+            #print (r, i)
             v = np.argmin(su, axis = 0)
-            r = v / 7
+            r = v // 7
+            #print (np.min(su), r)
             if r == i:
                 right += 1
 
     acc = right * 1.0 / (n * 3)
     print ("accuracy: %f %%" % (acc * 100.0))
 
-#Train()
-Recognize()
+if TRAINING:
+    Train()
+else:
+    Recognize()
